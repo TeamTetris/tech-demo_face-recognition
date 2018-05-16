c6af94ac5afe8c1ba7b52231deb37aa290c35819
Christian Friedow <christian@friedow.com>
2018-05-16 09:37:23 +0200

    Added response callback on api request

--- node_examples/face-analysis/script.js
+++ node_examples/face-analysis/script.js
@@ -1,13 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     var formData = new FormData();
     var xhr = new XMLHttpRequest();
+    var image = new Image(852, 480);
+    image.src = 'images/7.jpg';
     
     formData.append("api_key", faceplusplus_api_key());
     formData.append("api_secret", faceplusplus_api_secret());
     formData.append("return_attributes", 'eyestatus,gender,age,ethnicity,eyestatus,beauty,skinstatus');
-    formData.append("image_file", file);
+    formData.append("image_file", image);
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            console.log(xhr.response);
+        }
+    }
     
     xhr.open('post', url, true);
     xhr.setRequestHeader("Content-Type","multipart/form-data");
     xhr.send(formData);
+    console.log('Request send');
 });
