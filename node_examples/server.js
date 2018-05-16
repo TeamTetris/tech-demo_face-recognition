aa6f29885387f7ee08698ae4c35dafef7c4e7ee8
Christian Friedow <christian@friedow.com>
2018-05-16 11:06:44 +0200

    Added Face++ API CORS Proxy to server

--- node_examples/server.js
+++ node_examples/server.js
@@ -1,9 +1,23 @@
 var express = require('express');
+var request = require('request');
+
 var server = express();
 
 // Set directory for static files
 server.use('/camera', express.static('camera'));
 server.use('/face-analysis', express.static('face-analysis'));
+
+// CORS API proxy
+server.use('/api/face-analysis', function(req, res) {
+  if(req.method !== 'POST') {
+    return;
+  }
+  
+  var url = 'https://api-us.faceplusplus.com/facepp/v3/detect';
+  var response = request.post({uri: url, json: req.body});
+
+  req.pipe(response).pipe(res);
+});
 
 server.listen(3000, function () {
   console.log('Server listening on localhost:3000.');
